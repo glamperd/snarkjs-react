1628dc4caac87ea07b692709fb4878382fd691d9
Geoff Lamperd <glamperd@gmail.com>
2021-05-25 19:38:15 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {zKey} from 'snarkjs';
+
+async function contribute() {
+  const hash =
+        await zKey.contribute( "/data/c16_0404_amsimoes.zkey", { type: "mem" }, "contributor #2", "010a020b030d040f0r050g0tkalasdkasd", null);
+
+  //hashComponent.innerHTML = JSON.stringify(hash, null, 1);
+  console.log(`hash = ${JSON.stringify(hash)}`);
+}
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +28,9 @@ function App() {
         >
           Learn React
         </a>
+        <button id="bContrib" onClick={contribute}> Contribute </button>
+        <pre > Hash: <code id="hash"></code></pre>
+
       </header>
     </div>
   );
