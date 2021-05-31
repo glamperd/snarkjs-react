7fc0e730d6e81589c783ee34075816ffad36cfed
Geoff Lamperd <glamperd@gmail.com>
2021-05-31 19:26:52 +1000

    progress callback

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import {zKey} from 'snarkjs';
 
 async function contribute() {
+  const progressOptions = {
+    progressCallback: (val, total) => console.log(`snarkjs-react progress = ${val} of ${total}`)
+  }
   let fd =  { type: "mem" };
   const hash =
         await zKey.contribute( "/data/c16_0404_amsimoes.zkey", fd, 
-          "contributor #2", "010a020b030d040f0r050g0tkalasdkasd", console);
+          "contributor #2", "010a020b030d040f0r050g0tkalasdkasd", console, progressOptions);
 
   //hashComponent.innerHTML = JSON.stringify(hash, null, 1);
   console.log(`hash = ${JSON.stringify(hash)}`);
